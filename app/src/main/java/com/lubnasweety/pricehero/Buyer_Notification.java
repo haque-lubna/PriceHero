b2bb0a7be9e491c5c91271b18cd7e6a13d04f727
LubnaHaque <lubu7035@gmail.com>
2017-12-14 15:09:33 +0600

    latest update 14-12-17

---
+++ app/src/main/java/com/lubnasweety/pricehero/Buyer_Notification.java
@@ -0,0 +1,13 @@
+package com.lubnasweety.pricehero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Buyer_Notification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer__notification);
+    }
+}
